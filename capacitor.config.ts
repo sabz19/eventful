b20beed5f6e9ac1aea5942c279bf310eacf2a223
sabarishn <sabarishn@sabarishns-MBP.attlocal.net>
2025-03-22 19:15:18 -0400

    Eventful impl: Capacitor plugin correction added. Sync for ios integration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.aolsefl.eventful',
   appName: 'eventful',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
