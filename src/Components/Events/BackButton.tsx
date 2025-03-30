83b837d17fa74a89ccc99668f12d04d11783b74e
sabarishn <sabarishn@sabarishns-MBP.attlocal.net>
2025-03-30 15:56:27 -0400

    Eventful impl: Add backbutton functionality

---
+++ src/Components/Events/BackButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import 'animate.css';
+import '../Styles/Calendar.css';
+
+/**
+ * Basic functionality to implement a back button to go to the Home page 
+ * listing the events.
+ * @param props 
+ * @returns 
+ */
+function BackButton(props:any) {
+
+    const navigate = useNavigate(); 
+    const handleBackButtonClick = () => {
+        // This will take the user back to the previous page in the history stack
+        navigate(-1);
+    }
+    return(
+        <div>
+            <span onClick={handleBackButtonClick} className="back-button">
+                <FontAwesomeIcon icon={ faArrowLeft }></FontAwesomeIcon>
+            </span>
+        </div>
+    )
+}
+
+export default BackButton;
--- src/Routes/Event/EventDetails.tsx
+++ src/Routes/Event/EventDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import ThumbsButton from '../../Components/Events/ThumbsButton';
+import BackButton from '../../Components/Events/BackButton';
 import '../../Components/Styles/Event.css';
 import 'animate.css/animate.min.css';
 
@@ -12,15 +13,18 @@ function EventDetails(){
     console.log('event id = ', eventId);
     console.log(location.state.key.image);
     return (
-        <div className="event-container">
-            <div className="image-container">
-                <img className="event-image" src={location.state.key.image}/>
-            </div>
-            <h1 className="event-header"> { location.state.key.event }</h1>
-            <p className="event-description"> { location.state.key.description } </p>
-            <div className="like-dislike-container">
-                <ThumbsButton thumbsUp={true} />
-                <ThumbsButton thumbsDown={true} />
+        <div>
+            <BackButton />
+            <div className="event-container">
+                <div className="image-container">
+                    <img className="event-image" src={location.state.key.image}/>
+                </div>
+                <h1 className="event-header"> { location.state.key.event }</h1>
+                <p className="event-description"> { location.state.key.description } </p>
+                <div className="like-dislike-container">
+                    <ThumbsButton thumbsUp={true} />
+                    <ThumbsButton thumbsDown={true} />
+                </div>
             </div>
         </div>
 
